9f30a383d9fefa548e8d2b2cf758928f3901f6c2
Akshay Patel <akshay.ap95@gmail.com>
2025-02-28 11:41:26 +0100

    Remove unused import

--- contracts/ERC1271FallbackHandler.sol
+++ contracts/ERC1271FallbackHandler.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0
 pragma solidity ^0.8.0;
 import {Safe} from "@safe-global/safe-contracts/contracts/Safe.sol";
-import {ISignatureValidator} from "@safe-global/safe-contracts/contracts/interfaces/ISignatureValidator.sol";
 
 contract ERC1271FallbackHandler {
     // keccak256("SafeMessage(bytes message)");
